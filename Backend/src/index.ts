b2e0e0ad0a2c747b74698971f457c3043433aefa
Ayusome-git <ayush.ayush552@gmail.com>
2025-08-05 14:06:50 +0530

    init

---
+++ Backend/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+
+
+app.get("/",(req,res)=>{
+    res.json({
+        message:"server running"
+    })
+})
+
+
+app.listen(3000);
